8f8d7aa97d32f87760d4b1c3d4430aec5ae03f48
Cerus <zocks1703@web.de>
2020-06-12 00:05:49 +0200

    Add command channel check

--- src/main/java/xyz/spedcord/discordbot/util/CommandUtil.java
+++ src/main/java/xyz/spedcord/discordbot/util/CommandUtil.java
@@ -2,15 +2,27 @@ package xyz.spedcord.discordbot.util;
 
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import xyz.spedcord.discordbot.settings.GuildSettingsProvider;
 
 public class CommandUtil {
 
+    private static GuildSettingsProvider settingsProvider;
+
     private CommandUtil() {
+    }
+
+    public static void init(GuildSettingsProvider provider) {
+        settingsProvider = provider;
     }
 
     public static boolean isBotAdmin(Member member) {
         return member.hasPermission(Permission.ADMINISTRATOR) || member.getRoles().stream()
                 .anyMatch(role -> role.getName().equalsIgnoreCase("Spedcord Bot Admin"));
+    }
+
+    public static boolean isInCommandChannel(TextChannel channel) {
+        return channel.getIdLong() == settingsProvider.getGuildSettings(channel.getGuild().getIdLong()).getCommandChannelId();
     }
 
 }
